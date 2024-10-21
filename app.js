9979d4566739c4f0c14f524f79b7e88a8620242d
IsfandiyorOlimovvv <shoxagaming>
2024-10-21 22:26:27 +0500

    lesson-6

---
+++ app.js
@@ -0,0 +1,38 @@
+// let A = 23;
+// let B = 5;
+
+// let kesmalar = Math.floor(A / B);
+// let qolgan = A % B; 
+
+// console.log("Maksimal B kesma: " + kesmalar + " ta");
+// console.log("Qolgan bosh joy: " + qolgan);
+
+
+
+// let num = 16;
+// let daraja = Math.log2(num);
+
+// console.log("Bu son 2 ning " + daraja + "-darajasi.");
+
+
+
+// let a = 3;
+// let n = 4;
+// let result = 1;
+
+// for (let i = 0; i < n; i++) {
+//     result *= a;
+// }
+
+// console.log("Natija: " + a + " ning " + n + "-darajasi " + result);
+
+
+// let a = 2;
+// let b = 5;
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//     sum += i * i;
+// }
+
+// console.log("a dan b gacha bolgan sonlar kvadratlari yigindisi " + sum);
